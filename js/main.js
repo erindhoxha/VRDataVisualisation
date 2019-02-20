4b3f1b39b2c4fbef2f2b58348ccbd27d86bc5142
Erind Hoxha <932400893@cit3-501m-20.acg.ac.nz>
2019-02-21 10:45:01 +1300

    added europe button

--- js/main.js
+++ js/main.js
@@ -251,12 +251,70 @@ $(".australia-btn").on('click', function(){
     $("#svg-new-zealand").show();
     $(".myChart").attr('id', 'myChart');
     $("#myChart").show();
-    addChart();
+    addChartForAustralia();
     startSVGAnimationText($('#svg-australia'));
     startSVGAnimationText($('#svg-new-zealand'));
 })
 
-function addChart() {
+$(".europe-btn").on('click', function() {
+    $(".myChart").attr('id', 'myChart');
+    $("#myChart").show();
+    addChartForEurope()
+    $("#svg-australia").fadeOut(300);
+    $("#svg-new-zealand").fadeOut(300);
+})
+
+function addChartForEurope() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+      labels: 
+        ['Auckland', 'Wellington', 'Hamilton', 'Dunedin', 'Christchurch'],
+        datasets: [{
+          label: '# of Votes',
+          data: [15, 11, 5, 12, 24],
+          backgroundColor: [
+            'rgba(19, 165, 255, 0.721)',
+            'rgba(28, 28, 184, 0.721)',
+            'rgba(212, 119, 184, 0.721)',
+            'rgba(22, 221, 184, 0.721)',
+            'rgba(22, 221, 11, 0.721)',
+          ],
+          borderColor: [
+          ],
+          borderWidth: 0
+        }]
+      },
+      options: {
+        layout: {
+          padding: 30,
+          left: 10,
+          top: 5,
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              padding: 25,
+              beginAtZero: true,
+              display: false
+            },
+            display:false,
+          }]
+        }
+      },
+      layout: {
+        padding: {
+          left:50,
+          top:50,
+          bottom:0,
+          right:0
+        }
+      }
+    });
+}
+
+function addChartForAustralia() {
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
       type: 'doughnut',
