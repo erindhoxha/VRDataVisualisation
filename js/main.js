682af04a3450707a491a8844fa3df1e310aa734a
Erind Hoxha <erind_cbh@hotmail.com>
2019-02-21 23:30:01 +1300

    added

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@
 
 $(window).scroll(function () {
     var offset = $(".people-using-vr-rooms").offset().top;
-    var offset2 = $(".container-flash").offset().top;
     if ($(window).scrollTop() >= offset) {
         $("#nav-link-1").css('color','white');
+        activate(2);
         $("#nav-link").css('color','rgba(19, 165, 255, 0.721)');
     } else {
+        activate(1);
         $("#nav-link-1").css('color', 'rgba(19, 165, 255, 0.721)');
     }
 });
@@ -353,6 +354,7 @@ function Counter(obj){
 }
 
 $(document).ready(function(){
+activate(1);
   $('.number').each(function(){
 	  Counter($(this));
 	});
@@ -373,3 +375,42 @@ $(".vr-send").click(function () {
     },
         'slow');
 });
+
+  var $point_arr, $points, $progress, $trigger, active, max, tracker, val;
+
+  $trigger   = $('.trigger').first();
+  $points    = $('.progress-points').first();
+  $point_arr = $('.progress-point');
+  $progress  = $('.progress').first();
+
+  val     = +$points.data('current') - 1;
+  max     = $point_arr.length - 1;
+  tracker = active = 0;
+
+  function activate(index) {
+    if (index !== active) {
+      active       = index;
+      var $_active = $point_arr.eq(active)
+      
+      $point_arr
+        .removeClass('completed active')
+        .slice(0, active).addClass('completed')
+      
+      $_active.addClass('active-new');
+      
+      return $progress.css('width', (index / max * 100) + "%");
+    }
+  };
+
+  $points.on('click', 'li', function(event) {
+    var _index;
+    _index  = $point_arr.index(this);
+    tracker = _index === 0 ? 1 : _index === val ? 0 : tracker;
+    
+    return activate(_index);
+  });
+
+
+
+
+
