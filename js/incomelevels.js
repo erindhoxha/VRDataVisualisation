8e943ea88987d069f8870433126fa76c4435dbf9
Erind Hoxha <932400893@cit3-501m-20.acg.ac.nz>
2019-02-26 11:51:00 +1300

    added income levels

--- js/incomelevels.js
+++ js/incomelevels.js
@@ -12,7 +12,14 @@ $(document).ready(function() {
 	map = L.map('map', {
 		layers: [baseLayer]
 	}).setView([0.0, 0.0], 2);
-
+	
+	map.touchZoom.disable();
+	map.doubleClickZoom.disable();
+	map.scrollWheelZoom.disable();
+	map.boxZoom.disable();
+	map.keyboard.disable();
+	if (map.tap) map.tap.disable();
+	document.getElementById('map').style.cursor='default';
 	var incomeLevelTypes = ['OEC', 'NOC', 'UMC', 'MIC', 'LMC', 'LIC', 'HPC'];
 	var valueArray = [{"id":"HIC","value":"High income"},{"id":"HPC","value":"Heavily indebted poor countries (HIPC)"},{"id":"INX","value":"Not classified"},{"id":"LIC","value":"Low income"},{"id":"LMC","value":"Lower middle income"},{"id":"LMY","value":"Low & middle income"},{"id":"MIC","value":"Middle income"},{"id":"NOC","value":"High income: nonOECD"},{"id":"OEC","value":"High income: OECD"},{"id":"UMC","value":"Upper middle income"}];
 	var getMap = function (valueArray) {
@@ -42,8 +49,8 @@ $(document).ready(function() {
 
 	var styles = new L.StylesBuilder(incomeLevelTypes, {
 		displayName: incomeLevelToText,
-		color: colorFunction1,
-		fillColor: fillColorFunction1
+		color: 'rgba(19, 165, 255, 0.721)',
+		fillColor: 'rgba(19, 165, 255, 0.721)'
 	});
 
 	var options = {
@@ -79,146 +86,6 @@ $(document).ready(function() {
 	var incomeLayer = new L.ChoroplethDataLayer(incomeLevels, options);
 
 	map.addLayer(incomeLayer);
-
-	$('#legend').append(incomeLayer.getLegend({
-		className: 'well'
-	}));
-
-	var colorFunction = new L.HSLHueFunction(new L.Point(0,90), new L.Point(300000000,0), {outputSaturation: '100%', outputLuminosity: '30%'});
-	var fillColorFunction = new L.HSLHueFunction(new L.Point(0,90), new L.Point(300000000,0));
-
-	options = {
-		recordsField: null,
-		locationMode: L.LocationModes.COUNTRY,
-		codeField: 'CountryCode',
-		displayOptions: {
-			'2010': {
-				displayName: 'Telephone Lines',
-				color: colorFunction,
-				fillColor: fillColorFunction
-			}
-		},
-		layerOptions: {
-			fillOpacity: 0.7,
-			opacity: 1,
-			weight: 1
-		},
-		tooltipOptions: {
-			iconSize: new L.Point(100,65),
-			iconAnchor: new L.Point(-5,65)
-		},
-
-		onEachRecord: function (layer, record) {
-			var $html = $(L.HTMLUtils.buildTable(record));
-
-			layer.bindPopup($html.wrap('<div/>').parent().html(), {
-				maxWidth: 400,
-				minWidth: 400
-			});
-		}
-	};
-	var telephoneLinesLayer = new L.ChoroplethDataLayer(telephoneLines, options);
-
-	$('#legend').append(telephoneLinesLayer.getLegend({
-		className: 'well'
-	}));
-
-	var categories = ['1995','2000','2005','2010'];
-	var fillColorFunctionBars = new L.HSLLuminosityFunction(new L.Point(0,0.5), new L.Point(categories.length - 1,1), {outputHue: 0, outputSaturation: '100%'});
-	var styleFunction = new L.StylesBuilder(categories,{
-		displayName: function (index) {
-			return categories[index];
-		},
-		color: 'hsl(0,100%,20%)',
-		fillColor: fillColorFunctionBars,
-		minValue: 0,
-		maxValue: 20000000//300000000
-	});
-
-	options = {
-		recordsField: null,
-		locationMode: L.LocationModes.COUNTRY,
-		codeField: 'CountryCode',
-		chartOptions: styleFunction.getStyles(),
-		layerOptions: {
-			fillOpacity: 0.7,
-			opacity: 1,
-			weight: 1,
-			width: 6
-		},
-		tooltipOptions: {
-			iconSize: new L.Point(100,65),
-			iconAnchor: new L.Point(-5,65)
-		},
-
-		onEachRecord: function (layer, record) {
-			var $html = $(L.HTMLUtils.buildTable(record));
-
-			layer.bindPopup($html.wrap('<div/>').parent().html(), {
-				maxWidth: 400,
-				minWidth: 400
-			});
-		}
-	};
-
-	var telephoneLinesBarChart = new L.BarChartDataLayer(telephoneLines, options);
-
-	$('#legend').append(telephoneLinesBarChart.getLegend({
-		className: 'well',
-		title: 'Telephone Lines'
-	}));
-
-	var sfFillColorFunction = new L.HSLSaturationFunction(new L.Point(0,0.1), new L.Point(100,1), {outputHue: 220, outputLuminosity: '50%'});
-	var radiusFunction = new L.LinearFunction(new L.Point(0,2), new L.Point(100,10));
-
-	options = {
-		recordsField: null,
-		locationMode: L.LocationModes.COUNTRY,
-		codeField: 'CountryCode',
-		displayOptions: {
-			'2010': {
-				displayName: 'Solid Fuel Use (%)',
-				fillColor: sfFillColorFunction,
-				radius: radiusFunction
-			}
-		},
-		layerOptions: {
-			fillOpacity: 0.7,
-			opacity: 1,
-			weight: 1,
-			color: 'hsl(220,100%,25%)',
-			numberOfSides: 40,
-			dropShadow: true,
-			gradient: true
-		},
-		tooltipOptions: {
-			iconSize: new L.Point(100,65),
-			iconAnchor: new L.Point(-5,65)
-		},
-
-		onEachRecord: function (layer, record) {
-			var $html = $(L.HTMLUtils.buildTable(record));
-
-			layer.bindPopup($html.wrap('<div/>').parent().html(), {
-				maxWidth: 400,
-				minWidth: 400
-			});
-		}
-	};
-	var solidFuelLayer = new L.DataLayer(solidFuels, options);
-
-	$('#legend').append(solidFuelLayer.getLegend({
-		className: 'well'
-	}));
-
-	var overlays = {
-		"Income Levels": incomeLayer,
-		"Telephone Lines": telephoneLinesLayer,
-		"Telephone Lines by Year": telephoneLinesBarChart,
-		"Solid Fuel Use": solidFuelLayer
-	};
-
-	L.control.layers(baseMaps, overlays).addTo(map);
 });
 
 $(window).on('load', function() {
