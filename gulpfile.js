eef07b46d45c7b461392d0db0be6b2d5ee1a5ff3
Erind Hoxha <erind_cbh@hotmail.com>
2019-02-27 22:35:17 +1300

    getting there

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,34 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require("gulp-uglify");
+var pump = require("pump");
+
+sass.compiler = require('node-sass');
+
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('lib/*.js'),
+        uglify(),
+        gulp.dest('dist')
+    ],
+    cb
+  );
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
 function defaultTask(cb) {
     console.log("Done!");
+    console.log("Add another task!");
     cb();
 }
 
